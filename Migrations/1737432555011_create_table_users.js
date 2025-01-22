bc16645f578e0c11a2924f53498fa45f1733cad1
SyaPratama <inirasya16@gmail.com>
2025-01-22 07:07:17 +0700

    chore: adding ignore to upload

--- Migrations/1737432555011_create_table_users.js
+++
@@ -1,5 +0,0 @@
-export default { 
-    up:"CREATE TABLE users (id INT NOT NULL PRIMARY KEY, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
-    down:"DROP TABLE users" 
-}
-    
--- Migrations/1737436134152_create_table_games.js
+++
@@ -1,5 +0,0 @@
-export default { 
-    up:"CREATE TABLE games (id INT NOT NULL PRIMARY KEY, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
-    down:"DROP TABLE games" 
-}
-    
